66f2158bded010fbf28b338108ecb4b1768e6302
Shlomi <vgtvgy1@gmail.com>
2023-09-29 00:37:32 +0300

    convert to react components

--- src/App.js
+++ src/App.js
@@ -5,22 +5,21 @@ import './App.css';
 
 
 function App() {
-    // const gridRef = React.createRef();
-    //
-    // function clear_grid() {
-    //     if (gridRef.current) {
-    //         gridRef.current.clear();
-    //     }
-    // }
+    const gridRef = React.createRef();
+
+    function clear_grid() {
+        if (gridRef.current) {
+            gridRef.current.clear();
+        }
+    }
 
     return (
         <div className="App">
             <h1>Color the Grid</h1>
-            {/*<div className="button_pane" onClick={clear_grid}>*/}
-            {/*    <button>Clear grid</button>*/}
-            {/*</div>*/}
-            {/*<Grid  ref={gridRef} />*/}
-            <Grid  />
+            <div className="button_pane" onClick={clear_grid}>
+                <button>Clear grid</button>
+            </div>
+            <Grid  ref={gridRef} />
         </div>
     );
 }
--- src/Grid.js
+++ src/Grid.js
@@ -6,61 +6,78 @@ var last_tile_mouse_up = -1;
 var is_pressing = false;
 var is_coloring_or_erasing = true; // true = coloring, false = erasing
 
-function Grid() {
-    const [grid, setGrid] = useState(
-        Array(100)
-            .fill(false)
-            .map((_, index) => ({
-                is_colored: false,
-                is_start: index === 0,
-                is_finish: index === 99,
-            }))
-    );
 
-    const handleMouseDown = (index) => {
+class Grid extends React.Component {
+    constructor() {
+        super();
+        this.state = {
+            grid: Array(100)
+                .fill(false)
+                .map((_, index) => ({
+                    is_colored: false,
+                    is_start: index === 0,
+                    is_finish: index === 99,
+                })),
+        };
+        this.grid = this.state.grid;
+    }
+
+    handleMouseDown = (index) => {
         last_tile_mouse_down = index;
         is_pressing = true;
-        is_coloring_or_erasing = !grid[index].is_colored;
+        is_coloring_or_erasing = !this.grid[index].is_colored;
 
-        if (grid[index].is_start || grid[index].is_finish) return;
+        if (this.grid[index].is_start || this.grid[index].is_finish) return;
 
-        const newGrid = [...grid];
+        const newGrid = [...this.grid];
         newGrid[index].is_colored = is_coloring_or_erasing;
-        setGrid(newGrid);
+        this.setState({ grid: newGrid });
     };
 
-    const handleMouseUp = (index) => {
+    handleMouseUp = (index) => {
         last_tile_mouse_up = index;
         is_pressing = false;
     };
 
-    const handleMouseEnter = (index) => {
+    handleMouseEnter = (index) => {
         if (is_pressing) {
-            if (grid[index].is_start || grid[index].is_finish) return;
+            if (this.grid[index].is_start || this.grid[index].is_finish) return;
 
-            const newGrid = [...grid];
+            const newGrid = [...this.grid];
             newGrid[index].is_colored = is_coloring_or_erasing;
-            setGrid(newGrid);
+            this.setState({ grid: newGrid });
         }
-    }
+    };
 
-    return (
-        <div className="grid_container">
-            {
-                grid.map((tile, index) => (
-                <Tile
-                    key={index}
-                    index={index}
-                    is_colored={tile.is_colored.toString()}
-                    is_start={tile.is_start.toString()}
-                    is_finish={tile.is_finish.toString()}
-                    onMouseDown={() => handleMouseDown(index)}
-                    onMouseUp={() => handleMouseUp(index)}
-                    onMouseEnter={() => handleMouseEnter(index)}
-                />
-            ))}
-        </div>
-    );
+    clear = () => {
+        // Clear the grid by setting all tiles to uncolored
+        const clearedGrid = this.state.grid.map((tile) => ({
+            ...tile,
+            is_colored: false,
+        }));
+        this.setState({ grid: clearedGrid });
+    };
+
+    render() {
+        return (
+            <div className="grid_container">
+                {this.state.grid.map((tile, index) => (
+                    <Tile
+                        key={index}
+                        index={index}
+                        is_colored={tile.is_colored.toString()}
+                        is_start={tile.is_start.toString()}
+                        is_finish={tile.is_finish.toString()}
+                        onMouseDown={() => this.handleMouseDown(index)}
+                        onMouseUp={() => this.handleMouseUp(index)}
+                        onMouseEnter={() => this.handleMouseEnter(index)}
+                    />
+                ))}
+            </div>
+        );
+    }
 }
+
+
 
 export default Grid;
